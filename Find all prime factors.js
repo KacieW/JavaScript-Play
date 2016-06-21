1781a13fa8a65110d71c6c8edbfb2a59f59d6953
KacieW <kaciewang428@gmail.com>
2016-06-21 15:11:49 -0400

    Update Find all prime factors.js
    better solution than the first version

--- Find all prime factors.js
+++ Find all prime factors.js
@@ -1,33 +1,26 @@
 //How could you find all prime factors of a number?
-//first version
+//Better version
 
 function isPrimeFactor(n) {
-  var myArr = [];
+  var myArr = [1];
   var counter = 0;
+  var divisor = 2;
+  if(n===0||n==null){return console.log("Wrong number.")};
   if (n == 2 || n == 3) {
-    return console.log("prime factor is " + n);
-  } else if (n % 2 != 0 && n % 3 !=0 && n % 5 != 0 && n % 7 != 0) {
-    return console.log("prime factor is " + n);
+    return console.log("prime factor is " + n/n+", "+n);
+  } else if (n % 2 !== 0 && n % 3 !== 0 && n % 5 !== 0 && n % 7 !== 0) {
+    return console.log("prime factor is " + n/n+", "+n);
   } else {
-    while (n>=2&&n % 2 == 0) {
-      n = n / 2;
-      counter++;
+    while (n >= divisor) {
+      while (n % divisor === 0) {
+        n = n / divisor;
+        myArr.push(divisor);
+      }
+      divisor++;
     }
-    myArr=Array(counter).fill(2);
-    console.log(counter);
-    while(n>=3&&n%3==0){
-      n=n/3;
-      myArr.push(3);
-    }
-    while(n>=5&&n%5==0){
-      n=n/5;
-      myArr.push(5);
-    }
-    while(n>=7&&n%7==0){
-      n=n/7;
-      myArr.push(7);
-    }
+    return console.log("prime factors are " + myArr.join(","));
   }
-  return console.log("prime factors are " +myArr.join(","));
+
 }
-isPrimeFactor(63);
+isPrimeFactor();
+
