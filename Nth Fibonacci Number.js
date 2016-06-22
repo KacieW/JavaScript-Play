d8d3f03462b0d223804bf86c2f2c848bff073268
KacieW <kaciewang428@gmail.com>
2016-06-21 20:37:56 -0400

    Update Nth Fibonacci Number.js
    Add a method of O(n)

--- Nth Fibonacci Number.js
+++ Nth Fibonacci Number.js
@@ -7,3 +7,19 @@ function Fib(n){
   }
   return Fib(n-1)+Fib(n-2);
 }
+
+/* better time complexy
+function fibo(n){
+  var fiboArr = [0,1];
+  if(n<=1){
+    return console.log(1);
+  }else{
+    for(var i=2; i<=n;i++){
+      fiboArr[i] = fiboArr[i-1]+fiboArr[i-2];
+    }
+    return console.log(fiboArr[n]);
+  }
+  
+}
+fibo(6);
+*/
