168aaaf9bd550ad02cb7868693d5294eadf98b62
KacieW <kaciewang428@gmail.com>
2016-08-26 15:57:27 -0400

    Update HashMap-sum-to-given-number.js

--- HashMap-sum-to-given-number.js
+++ HashMap-sum-to-given-number.js
@@ -12,3 +12,23 @@ for(var j=0;j<A.length-1; j++){
      map.delete(A[j]);
    }
 }
+
+
+//better way, referenced from http://thatjsdude.com/interview/js1.html#SumOfLargestTwo
+function sumFinder(arr, sum){
+  var differ = {}, 
+      len = arr.length,//7
+      substract;
+  
+  for(var i =0; i<len; i++){
+     substract = sum - arr[i];
+     if(differ[substract]){
+       var a = substract+'--'+arr[i];
+       console.log(a);
+       }       
+     else{
+       differ[arr[i]] = true;}
+  }
+}
+
+sumFinder([6,4,3,2,1,7], 9);
