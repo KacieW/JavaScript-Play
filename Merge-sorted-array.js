a9aa6f121382989f31900a2c5ea05d57f5aa3e3d
KacieW <kaciewang428@gmail.com>
2016-08-28 15:21:44 -0400

    Update Merge-sorted-array.js

--- Merge-sorted-array.js
+++ Merge-sorted-array.js
@@ -10,3 +10,31 @@ function mergeSort(a, b){
 }
 
 console.log(mergeSort([1,3,3,4,7],  [1,2,5,5,7,9]));
+
+function mergeTwo(arr1, arr2){
+  var res = [];
+	while(arr1.length>0 && arr2.length>0){
+		if(arr1[0]<arr2[0]){
+			res.push(arr1.shift());
+		}
+		if(arr1[0]>arr2[0]){
+			res.push(arr2.shift());
+		}
+		if(arr1[0]==arr2[0]){
+			res.push(arr1.shift());
+			res.push(arr2.shift());
+		}
+	}
+	
+	if(arr1.length==0){
+		res = res.concat(arr2);
+
+	}else{
+		res = res.concat(arr1);
+	}
+	return res;
+}
+
+var arr1 = [1,2,5,7,7,9];
+var arr2 = [1,90];
+document.getElementById('d').innerHTML = mergeTwo(arr1, arr2);
