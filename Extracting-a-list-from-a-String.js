94634e17878aa1a1eb2b0f23075c0b3923f948f7
KacieW <kaciewang428@gmail.com>
2017-04-11 17:36:39 -0700

    Update Extracting-a-list-from-a-String.js

--- Extracting-a-list-from-a-String.js
+++ Extracting-a-list-from-a-String.js
@@ -1,5 +1,8 @@
 //Extracting a list from a String, from Javascript cookbook
 
+//Step 1, find the string and extract it from the original string
+//Step 2, turning the string into an array and trim the spaces.
+
 var str = "This is a list. This is a list of items: apply, cherry, lime, orange. This is a list.";
 
 var start = str.indexOf(":");
@@ -9,3 +12,5 @@ var newStr = str.slice(start+1, end).split(",");
 
 var result = newStr.map(function(x){return x.trim();})
 console.log(result);
+
+
