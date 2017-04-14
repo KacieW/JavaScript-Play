a3cae0155741d3a6e23cfdb4f2f1c0e57ba3ef6d
KacieW <kaciewang428@gmail.com>
2017-04-14 13:27:24 -0700

    Update 2.5 Applying-a -function-against-each-array-elemt.js

--- Cookbook/2.5 Applying-a -function-against-each-array-elemt.js
+++ Cookbook/2.5 Applying-a -function-against-each-array-elemt.js
@@ -1,8 +1,11 @@
 var arr = ["a", "aa", "bb", "ab", "d"];
-
+//forEach change the original arr
 arr.forEach(function(item, index, arr){
 	if(item=="aa"){
 		arr[index] = "7";
 	}
 });
-console.log(arr);
+
+//apply the change to a new array.
+var newArr = arr.map(function(x){if(x=="aa"){return "7";}else{return x;}});
+console.log(newArr);
